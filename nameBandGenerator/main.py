2181ca732ff49295d1bae6660acd00c064fc8f01
Dinarbrooke <bethmij@gmail.com>
2024-01-15 11:07:24 +0530

    added bandNameGenerator

---
+++ nameBandGenerator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up?\n")
+pet_name = input("What's your pet's name\n")
+print("Your Band name could be "+city+" "+pet_name)
+
+
