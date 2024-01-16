f5e8d7c4a967b2602e0d80fbcd560ed842468788
Dinarbrooke <bethmij@gmail.com>
2024-01-16 10:44:50 +0530

    added rock paper scissors

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-print("The Love Calculator is calculating your score...")
-name1 = input().lower()  # What is your name?
-name2 = input().lower()  # What is their name?
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
+import random
 
-true_count = 0
-love_count = 0
-
-for name_letter in range(0, len(name1)):
-    true_count += 1 if name1[name_letter] in "true" else 0
-    love_count += 1 if name1[name_letter] in "love" else 0
-
-for name_letter in range(0, len(name2)):
-    true_count += 1 if name2[name_letter] in "true" else 0
-    love_count += 1 if name2[name_letter] in "love" else 0
-
-
-final_count = str(true_count) + str(love_count)
-
-print(f"Your score is {final_count}.")
+print(random.randint(0,100))
