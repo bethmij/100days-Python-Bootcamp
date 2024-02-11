c195009dd3838daf3fe5124dc49cf0bcf356bd2e
Dinarbrooke <bethmij@gmail.com>
2024-02-11 13:22:52 +0530

    blackjack added

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -11,20 +11,21 @@ while not is_start_new:
     for op in operator:
         print(op)
 
-    operator_choice = input("Pick an operator : ")
+    while not is_continue:
+        operator_choice = input("Pick an operator : ")
 
-    if operator_choice in operator:
-        while not is_continue:
+        if operator_choice in operator:
             num2 = input("What's the second number? : ")
             cal_num = "{:.1f}".format(eval(num1 + operator_choice + num2))
 
             print(f"{num1} + {num2} = {cal_num}")
-            option = input(f"Type 'y' to continue calculating with {cal_num} or type 'n' to start new calculation : ")
+            option = input(
+                f"Type 'y' to continue calculating with {cal_num} or type 'n' to start new calculation : ")
 
             if option.lower() == "y":
                 num1 = cal_num
                 continue
             elif option.lower() == "n":
                 break
-    else:
-        print("Invalid Input! Try Again")
+        else:
+            print("Invalid Input! Try Again")
