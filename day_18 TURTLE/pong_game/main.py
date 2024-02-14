1368bb8c2636e8386bd1601f0bc2bea3b34ce845
Dinarbrooke <bethmij@gmail.com>
2024-02-14 10:49:16 +0530

    pong create another paddle

--- day_18 TURTLE/pong_game/main.py
+++ day_18 TURTLE/pong_game/main.py
@@ -1,6 +1,5 @@
 from turtle import Screen, Turtle
-
-POSITION = [(370, 0), (-370, 0)]
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -8,26 +7,14 @@ screen.bgcolor('black')
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddle = Turtle(shape='square')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.color('white')
-paddle.goto(370, 0)
-
-
-def move_paddle_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def move_paddle_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+right_paddle = Paddle((370, 0))
+left_paddle = Paddle((-380, 0))
 
 screen.listen()
-screen.onkey(move_paddle_up, "Up")
-screen.onkey(move_paddle_down, "Down")
+screen.onkey(right_paddle.move_paddle_up, "Up")
+screen.onkey(right_paddle.move_paddle_down, "Down")
+screen.onkey(left_paddle.move_paddle_up, "w")
+screen.onkey(left_paddle.move_paddle_down, "s")
 
 is_game_on = True
 while is_game_on:
