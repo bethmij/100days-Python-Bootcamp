1a83041fdfd4b9137823dc4bd46a727c77397adb
Dinarbrooke <bethmij@gmail.com>
2024-02-14 11:08:26 +0530

    pong moving ball

---
+++ day_18 TURTLE/pong_game/ball.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('white')
+
+    def move_ball(self):
+        # is_move = True
+        # for _ in range(10):
+        self.penup()
+        self.setheading(30)
+        self.forward(10)
+
+    def detect_wall_collisio
--- day_18 TURTLE/pong_game/main.py
+++ day_18 TURTLE/pong_game/main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 right_paddle = Paddle((370, 0))
 left_paddle = Paddle((-380, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.move_paddle_up, "Up")
@@ -19,5 +22,7 @@ screen.onkey(left_paddle.move_paddle_down, "s")
 is_game_on = True
 while is_game_on:
     screen.update()
+    time.sleep(0.1)
+    ball.move_ball()
 
 screen.exitonclick()
