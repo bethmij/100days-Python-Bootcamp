73da6256b1f0df76a1168c4773a70288d1dec9fb
Dinarbrooke <bethmij@gmail.com>
2024-02-18 14:59:21 +0530

    Squirrel Census Data Analysis added

---
+++ day_18 TURTLE/Squirrel Census Data Analysis/main.py
@@ -0,0 +1,17 @@
+import pandas
+
+PLACEHOLDER = 'Primary Fur Color'
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240218.csv")
+data_dict = {"Fur Color": [], "Count": []}
+
+unique_color = (data[PLACEHOLDER].unique()).tolist()
+
+for color in unique_color[1:]:
+    count = (data[data[PLACEHOLDER] == color]).size
+    data_dict['Fur Color'].append(color)
+    data_dict['Count'].append(count)
+
+
+data = pandas.DataFrame(data_dict)
+data.to_csv('data.csv')
+
