fa5ff0da5b8a18cd96d2ccb55f2ff591f30a3095
Dinarbrooke <bethmij@gmail.com>
2024-02-18 14:08:17 +0530

    reading csv

--- day_18 TURTLE/reading_csv/main.py
+++ day_18 TURTLE/reading_csv/main.py
@@ -1,6 +1,17 @@
 import csv
+import pandas
 
-with open("weather_data.csv") as file:
-    data = csv.reader(file)
-    temperature
-    for datum in data:
+# with open("weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temperature = []
+#     for datum in data:
+#         if datum[1] != 'temp':
+#             temperature.append(int(datum[1]))
+#
+#     print(temperature)
+
+data = pandas.read_csv("weather_data.csv")
+temperatures_max = data['temp'].max()
+# average_temp = sum(temperatures)//len(temperatures)
+print(temperatures_max)
+
