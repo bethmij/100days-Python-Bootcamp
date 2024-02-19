630b59c2e6081b4428e03b3382c9552fa0eef255
Dinarbrooke <bethmij@gmail.com>
2024-02-19 14:38:54 +0530

    NATO-alphabet update

---
+++ day_18 - day_26 TURTLE/NATO_alphabet/main.py
@@ -0,0 +1,12 @@
+import pandas
+
+data = pandas.read_csv('nato_phonetic_alphabet.csv')
+
+nato_format_dict = {value.letter: value.code for (key, value) in data.iterrows()}
+
+user_input = input('Enter a word : ').upper()
+
+nato_nam = [nato_format_dict[letter] for letter in user_input]
+print(nato_nam)
+
+
