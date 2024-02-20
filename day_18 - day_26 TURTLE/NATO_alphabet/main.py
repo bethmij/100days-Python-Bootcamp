90e9edc2df12f4c0e2863e1b9120c1f463c4fafe
Dinarbrooke <bethmij@gmail.com>
2024-02-20 19:35:07 +0530

    tkinter playground updated

--- day_18 - day_26 TURTLE/NATO_alphabet/main.py
+++ day_18 - day_26 TURTLE/NATO_alphabet/main.py
@@ -1,13 +1,21 @@
 import pandas
 
-data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-nato_format_dict = {value.letter: value.code for (key, value) in data.iterrows()}
+# data = pandas.read_csv('nato_phonetic_alphabet.csv')
+#
+# nato_format_dict = {value.letter: value.code for (key, value) in data.iterrows()}
+#
+# user_input = input('Enter a word : ').upper()
+#
+# nato_nam = [nato_format_dict[letter] for letter in user_input]
+#
+# data = [23,432,34,'fgfg',324,True]
+#
+# print(data)
 
-user_input = input('Enter a word : ').upper()
-
-nato_nam = [nato_format_dict[letter] for letter in user_input]
-
-print(nato_nam)
+def add(*args):
+    return sum(args)
 
 
+total = add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(total)
