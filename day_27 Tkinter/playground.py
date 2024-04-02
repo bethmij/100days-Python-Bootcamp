a513b60344561a6b616a703e42b55c59ea6c3e1b
bethmij <bethmij@gmail.com>
2024-04-02 19:38:41 +0530

    tkinter playground updated

---
+++ day_27 Tkinter/playground.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+window = Tk()
+window.title("Playground")
+window.minsize(width=500, height=500)
+
+label = Label(text="I am a label", font=('Arial', 20, 'normal'))
+label.pack()
+
+
+def change_text():
+    msg = input_field.get()
+    label['text'] = msg
+
+
+button = Button(text='click me', command=change_text)
+button.pack()
+
+input_field = Entry()
+input_field.pack()
+
+
+window.mainloop()
