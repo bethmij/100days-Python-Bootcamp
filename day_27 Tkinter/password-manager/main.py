64d1063312239694f881adb53577b029a13ec7d5
bethmij <bethmij@gmail.com>
2024-04-05 12:52:39 +0530

    PASSWORD GENERATOR added

--- day_27 Tkinter/password-manager/main.py
+++ day_27 Tkinter/password-manager/main.py
@@ -1,10 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 FONT = ("Arial", 11)
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+    pass_letters = [random.choice(letters) for _ in range(nr_letters)]
+    pass_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    pass_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_list = pass_letters + pass_symbols + pass_numbers
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    input_password.delete(0, END)
+    input_password.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_details():
@@ -59,7 +84,7 @@ text_password = Label(text="Password", font=FONT)
 text_password.grid(row=3, column=0)
 
 # Buttons
-button_generate = Button(text="Generate", width=10)
+button_generate = Button(text="Generate", width=10, command=generate_password)
 button_generate.grid(row=3, column=2)
 
 button_add = Button(text="Add", width=36, command=add_details)
