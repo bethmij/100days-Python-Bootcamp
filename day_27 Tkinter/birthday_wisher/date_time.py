4d957fb9c121f0f0aeec6a5797822aff35cc5a67
bethmij <bethmij@gmail.com>
2024-04-06 11:11:37 +0530

    add date_time

---
+++ day_27 Tkinter/birthday_wisher/date_time.py
@@ -0,0 +1,11 @@
+import datetime as dt
+
+date = dt.datetime.now()
+year = date.year
+month = date.month
+day = date.day
+print(year, month, day)
+
+birthday = dt.datetime(2002, 5, 30, 15, 20)
+print(birthday)
+
